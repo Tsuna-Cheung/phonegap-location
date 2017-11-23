1ba7b3357886207baea09c5c70f677a772bd3b19
Tsuna-Cheung <237971264@qq.com>
2017-11-23 14:59:11 +0000

    3

--- www/index.js
+++ www/index.js
@@ -1,8 +1,17 @@
+var watchID;
+
+var locationOptions = { 
+	maximumAge: 10000, 
+	timeout: 6000, 
+	enableHighAccuracy: true 
+};
+
 //when the jQuery Mobile page is initialised
 $(document).on('pageinit', function() {
 	
-	//set up listener for button click
-	$(document).on('click', getPosition);
+	//set up listener for button clicks
+	$('#startLocationButton').on('click', updatePosition);
+	$('#stopLocationButton').on('click', stopPosition);
 	
 	//change time box to show message
 	$('#time').val("Press the button to get location data");
@@ -10,14 +19,25 @@ $(document).on('pageinit', function() {
 });
 
 
-//Call this function when you want to get the current position
-function getPosition() {
+
+//Call this function when you want to watch for chnages in position
+function updatePosition() {
 	
 	//change time box to show updated message
 	$('#time').val("Getting data...");
 	
 	//instruct location service to get position with appropriate callbacks
-	navigator.geolocation.getCurrentPosition(successPosition, failPosition);
+	watchID = navigator.geolocation.watchPosition(successPosition, failPosition, locationOptions);
+}
+
+//Call this function when you want to watch for chnages in position
+function stopPosition() {
+	
+	//change time box to show updated message
+	$('#time').val("Press the button to get location data");
+	
+	//instruct location service to get position with appropriate callbacks
+	navigator.geolocation.clearWatch(watchID);
 }
 
 
@@ -31,12 +51,12 @@ function successPosition(position) {
 	//lets get some stuff out of the position object
 	var time = position.timestamp;
 	var latitude = position.coords.latitude;
-    var Longitude = position.coords.longitude;
+	var longitude = position.coords.longitude;
 	
 	//OK. Now we want to update the display with the correct values
 	$('#time').val("Recieved data at " + time);
-	$('#lattext').val("Recieved data at " + latitude);
-    $('#longtext').val("Recieved data at " + Longitude);
+	$('#lattext').val(latitude);
+	$('#longtext').val(longitude);
 	
 }
 
